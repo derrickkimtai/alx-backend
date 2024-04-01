b6649b46b66aefc770dd6b93ca77df86ba7c9cfb
derrickkimtai <derrickmoio92@gmail.com>
2024-04-01 19:11:10 +0300

    pagination

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+0-simple_helper_function
+"""
+
+from typing import Tuple, List
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    index_range
+    """
+    return ((page - 1) * page_size, page * page_size)
