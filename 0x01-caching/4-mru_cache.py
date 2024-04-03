451dddbc5183590c1918eedb654e0fb430e4080d
derrickkimtai <derrickmoio92@gmail.com>
2024-04-03 23:32:52 +0300

    cache

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" MRU caching
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """ MRU caching
+    """
+    def __init__(self):
+        """ Initiliaze
+        """
+        super().__init__()
+        self.cache_data = {}
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                last_key = next(reversed(self.cache_data))
+                del self.cache_data[last_key]
+                print("DISCARD: {}".format(last_key))
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
