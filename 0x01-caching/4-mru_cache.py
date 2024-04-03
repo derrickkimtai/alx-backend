a0b0ab86724bc2362d9d4037817c2d167f7be39c
derrickkimtai <derrickmoio92@gmail.com>
2024-04-03 23:37:34 +0300

    4

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -5,27 +5,33 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """ MRU caching
+    """ MRU cache class
     """
+
     def __init__(self):
-        """ Initiliaze
+        """ Constructor method
         """
         super().__init__()
-        self.cache_data = {}
+        self.mru_keys = []
 
     def put(self, key, item):
-        """ Add an item in the cache
+        """ Put method
         """
         if key and item:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                last_key = next(reversed(self.cache_data))
-                del self.cache_data[last_key]
-                print("DISCARD: {}".format(last_key))
             self.cache_data[key] = item
+            if key in self.mru_keys:
+                self.mru_keys.remove(key)
+            self.mru_keys.append(key)
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                mru_key = self.mru_keys.pop(-2)
+                del self.cache_data[mru_key]
+                print("DISCARD: {}".format(mru_key))
 
     def get(self, key):
-        """ Get an item by key
+        """ Get method
         """
         if key in self.cache_data:
+            self.mru_keys.remove(key)
+            self.mru_keys.append(key)
             return self.cache_data[key]
         return None
