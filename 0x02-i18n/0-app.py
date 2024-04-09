9a8fb170f4a427282659a98e6817901f3d379120
derrickkimtai <derrickmoio92@gmail.com>
2024-04-09 20:20:06 +0300

    flask

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Basic Flask app, Basic Babel setup
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """ Main route
+
+    Returns:
+        str: template
+    """
+    return render_template('0-index.html')
+
+if __name__ == '__main__':
+    app.run()
